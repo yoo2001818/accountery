a29e6313dfe799e5d3e229dc823ef265d4af381e
Duknam Yoo <yoo2001818@gmail.com>
2017-06-13 23:13:01 +0900

    Fix drop down offset algorithm to handle scrolls

--- src/component/dropDown.js
+++ src/component/dropDown.js
@@ -46,24 +46,33 @@ export default class DropDown extends Component {
     // Don't do layout if one of the nodes are missing.
     if (this.root == null || this.node == null) return;
     let { innerWidth, innerHeight } = window;
-    let nodeRect = this.node.getBoundingClientRect();
     this.layoutValid = true;
     let x, y;
+    let nodeRect = this.node.getBoundingClientRect();
     let alignRect = this.root.getBoundingClientRect();
+    // We need this to get exact location of the node...
+    let body = document.body;
+    let docEl = document.documentElement;
+    let scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+    let scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+    let clientTop = docEl.clientTop || body.clientTop || 0;
+    let clientLeft = docEl.clientLeft || body.clientLeft || 0;
+    let diffTop = scrollTop - clientTop;
+    let diffLeft = scrollLeft - clientLeft;
     if (this.props.top) {
-      y = alignRect.top - nodeRect.height;
+      y = alignRect.top + diffTop - nodeRect.height;
       if (y < 0) {
-        y = Math.min(innerHeight - nodeRect.height, alignRect.bottom);
+        y = Math.min(innerHeight - nodeRect.height, alignRect.bottom + diffTop);
       }
     } else {
-      y = alignRect.bottom;
-      if (y + nodeRect.height > innerHeight) {
-        y = Math.max(0, alignRect.top - nodeRect.height);
+      y = alignRect.bottom + diffTop;
+      if (y + nodeRect.height > innerHeight + diffTop) {
+        y = Math.max(0, alignRect.top + diffTop - nodeRect.height);
       }
     }
-    x = alignRect.left;
-    if (x + nodeRect.width > innerWidth) {
-      x = Math.max(0, alignRect.right - nodeRect.width);
+    x = alignRect.left + diffLeft;
+    if (x + nodeRect.width > innerWidth + diffLeft) {
+      x = Math.max(0, alignRect.right + diffLeft - nodeRect.width);
     }
     this.setState({ x, y });
   }
