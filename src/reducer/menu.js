e6b56d5a5bde1c0ea46f1e8557eb02d50a8272dd
Duknam Yoo <yoo2001818@gmail.com>
2019-12-29 23:59:07 +0900

    Wipe previous project

--- src/reducer/menu.js
+++
@@ -1,9 +0,0 @@
-import { handleActions } from 'redux-actions';
-
-import { SET_OPEN } from '../action/menu';
-
-export default handleActions({
-  [SET_OPEN]: (state, action) => Object.assign({}, state, {
-    open: action.payload,
-  }),
-}, { open: false });
